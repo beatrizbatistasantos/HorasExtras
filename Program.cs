dd833b917cb363aa99e67aab460d61d303cd44f8
Beatriz Batista <byah24@outlook.com>
2025-05-12 10:54:58 -0300

    Programa começado

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// Exercício HorasExtras
+// Escreva uma função que calcule o salário de um funcionário 
+// a partir salário-hora base, o total de horas trabalhadas e 
+// número de horas-extras executadas. 
+// Considere que cada hora-extra é paga com acréscimo de 40%.
+
+//Solicita entrada...
+decimal salarioHoraBase, salario;
+int horasTrabalhadas, numeroHorasExtras;
+
+Console.WriteLine("Digite o valor do Salário Hora Base:");
+salarioHoraBase = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite a quantidade de Horas Trabalhadas:");
+horasTrabalhadas = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite a quantidade de Horas Extras:");
+salarioHoraBase = Convert.ToDecimal(Console.ReadLine());
+
+
+//Receber a entrada do usuário e converter...
+//Executar função CalculaSalario
+void CalculaSalario()
+{
+    
+}
+salario = CalculaSalario(.......);
+//Exibir saida
+
+Console.WriteLine($"O salário é {salario:C2}");
+
+_________ CalculaSalario(.......)
+{
+    //lógica
+}
